7ab2a3001960ad392e610813b8b35cb6dbf51b8d
Ankit Singh <ankit.singh.zm@gmail.com>
2024-02-01 23:17:23 +0530

    added better file upload input

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,8 @@
-import { writeFile } from "fs/promises";
-import Image from "next/image";
-import { join } from "path";
+'use client'
+
+import Upload from "./actions/Upload";
 
 export default function Home() {
-
-  async function upload(data: FormData) {
-    'use server'
-
-    const file: File | null = data.get('file') as unknown as File
-
-    if (!file) {
-      throw new Error('No file upload')
-    }
-
-    const bytes = await file.arrayBuffer()
-    const buffer = Buffer.from(bytes)
-
-    // writing to filesystem temporarly
-    const path = join('/', 'tmp', file.name)
-    await writeFile(path, buffer)
-    console.log(`open ${path} to see uploaded file`);
-    
-  }
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
@@ -33,7 +14,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://github.com/ankitzm"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -44,10 +25,22 @@ export default function Home() {
       </div>
 
       <div className="mt-14 md:mt-16 lg:mt-20">
-        main
-        <form action={upload}>
-          <input type="file" name="file" />
-          <input type="submit" value="Upload File" />
+        <form action={Upload}>
+          <label htmlFor="uploadFile"
+            className="bg-white backdrop-blur-sm bg-opacity-20 text-center rounded w-full sm:w-[360px] min-h-[160px] py-4 px-4 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif] m-4">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 mb-6 fill-white" viewBox="0 0 24 24">
+              <path
+                d="M22 13a1 1 0 0 0-1 1v4.213A2.79 2.79 0 0 1 18.213 21H5.787A2.79 2.79 0 0 1 3 18.213V14a1 1 0 0 0-2 0v4.213A4.792 4.792 0 0 0 5.787 23h12.426A4.792 4.792 0 0 0 23 18.213V14a1 1 0 0 0-1-1Z"
+                data-original="#000000" />
+              <path
+                d="M6.707 8.707 11 4.414V17a1 1 0 0 0 2 0V4.414l4.293 4.293a1 1 0 0 0 1.414-1.414l-6-6a1 1 0 0 0-1.414 0l-6 6a1 1 0 0 0 1.414 1.414Z"
+                data-original="#000000" />
+            </svg>
+            <p className="text-white font-semibold text-sm">Drag & Drop or <span className="text-[#4498f2]">Choose file</span> to
+              upload</p>
+            <input type="file" id='uploadFile' name="file" className="hidden" />
+          </label>
+          <input type="submit" value="Upload File" className="bg-white backdrop-blur-sm bg-opacity-20 p-2 font-semibold rounded w-full" />
         </form>
       </div>
 
