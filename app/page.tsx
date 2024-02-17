c08ed4d8c8f6b877b6c3593c5c5377c980fd97d4
Ankit Singh <ankit.singh.zm@gmail.com>
2024-02-17 19:25:25 +0530

    color fix on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -22,13 +22,13 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 max-w-screen">
+    <main className="flex min-h-screen flex-col items-center p-24 max-w-screen bg-black text-white">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="fixed left-0 top-0 flex flex-col w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 p-2 lg:p-4 lg:dark:bg-zinc-800/30">
+        <h1 className="fixed left-0 top-0 flex flex-col w-full justify-center border-b border-gray-500 bg-gradient-to-b from-zinc-800 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 p-2 lg:p-4 lg:dark:bg-zinc-800/30">
           <p className="text-lg font-bold">share-on-network</p>
           <p>Share files seamlessly using Nextjs on local network</p>
         </h1>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-20 w-full items-end justify-center bg-gradient-to-t from-zinc-800 dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://github.com/ankitzm"
